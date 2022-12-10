52049a96d45f5bc11c5f41acba1fc60d25859277
arvene <arvene241@gmail.com>
2022-12-10 17:36:49 +0800

    add link

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -35,9 +35,11 @@ const Footer = () => (
         <div className="mb-[50px] h-[2px] bg-white opacity-10" />
 
         <div className="flex items-center justify-between flex-wrap gap-4">
-          <h4 className="font-extrabold text-[24px] text-white">
-            METAVERUS
-          </h4>
+          <a href='#'>
+            <h4 className="font-extrabold text-[24px] text-white">
+              METAVERUS
+            </h4>
+          </a>
           <p className="font-normal text-[14px] text-white opacity-50">
             Copyright © 2021 - 2022 Metaversus. All rights reserved.
           </p>
